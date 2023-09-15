98998e5d5cf6aa205c628a1da678e374b1bcbb1b
Rhayadh Zaparoli <120688001+rhayadhzaparoli@users.noreply.github.com>
2023-09-14 21:29:39 -0300

    Create main.java

---
+++ LIN3/ex005/Main/main.java
@@ -0,0 +1,21 @@
+package LIN3.ex005.Main;
+/*Crie um pequeno projeto ("Aula05_01") para controlar uma aula, com todas as entidades envolvidas nela. O projeto deve ter, ao menos, as seguintes classes:
+- Professor (nome, email e registro);
+- Aluno (nome, email e matrícula);
+- Disciplina (codigo, ementa);
+- Materia (topico, descricao);
+- Aula (Professor, Aluno[], Disciplina, Materia);
+- Principal (onde deverá ter a função "main").
+
+Após desenvolver todas as classes e seus atributos, instancie os objetos necessários a fim de que seja possível armazenar e apresentar, através de uma instância da classe Aula, todas as informações pertinentes à aula de uma disciplina qualquer, na qual haja, ao menos, 5 alunos. Em seguida, exiba todas as informações armazenadas (nome da matéria, nome do professor, código da disciplina e listagem de alunos presentes).
+
+Notas:
+- para armazenar vários alunos, pesquise sobre a classe "ArrayList"; 
+- para percorrer o array, utilizar o comando "foreach" ==> "for (Object o: lst)";
+- sinta-se livre para utilizar o conceito de herança envolvendo as classes Professor e Aluno.
+
+
+ */
+public class main {
+    
+}
